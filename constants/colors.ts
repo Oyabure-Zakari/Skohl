d00eede108f8c11c570b5d768afc8ea74bf2a107
Oyabure Zakari <oyabure205@gmail.com>
2025-11-01 22:42:04 +0100

    create colors and images constant files

---
+++ constants/colors.ts
@@ -0,0 +1,12 @@
+const COLORS = {
+  lightGrey: "#EEEEEE",
+  darkGrey: "#9D9D9E",
+  darkBlue: "#050110",
+  yellow: "#FFC60C",
+  purple: "#541EDC",
+  green: "#2975A1",
+  white: "#FCFCFC",
+  red: "#EE3629"
+};
+
+export default COLORS;
---
+++ constants/images.ts
@@ -0,0 +1,5 @@
+const IMAGES = {
+  letter0: require("../assets/images/LetterO.png"),
+};
+
+export default IMAGES;
