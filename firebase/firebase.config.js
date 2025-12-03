95ca4ebfe786507e9e6331659289e12b5a09fea6
Oyabure Zakari <oyabure205@gmail.com>
2025-12-03 13:04:15 +0100

    FB configuration

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,8 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCkFYV1zdnfHN1jEtk-vXQ-wz76D7DDDFs",
   authDomain: "skohl-8e61e.firebaseapp.com",
@@ -15,5 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
+export const auth = getAuth(app); // Initialize Firebase Authentication and get a reference to the service
+export const db = getFirestore(app); // Initialize Cloud Firestore and get a reference to the service
+export const storage = getStorage(app); // Initialize Cloud Storage and get a reference to the service
