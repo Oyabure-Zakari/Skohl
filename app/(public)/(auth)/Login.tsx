3123c4c6f673b348f5f7e96924d6872eae8adc84
Oyabure Zakari <oyabure205@gmail.com>
2025-12-10 20:54:48 +0100

    Refactor login functionality

--- app/(public)/(auth)/Login.tsx
+++ app/(public)/(auth)/Login.tsx
@@ -1,10 +1,9 @@
 import React, { useRef, useState } from "react";
-import { TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
 
 import { Redirect, useRouter } from "expo-router";
 
 import LoginImage from "@/components/login/LoginImage";
-import CustomButton from "@/components/reuseableComponents/CustomButton";
 import CustomKeyboard from "@/components/reuseableComponents/CustomKeyboard";
 import FormErrorText from "@/components/reuseableComponents/FormErrorText";
 import FormFooter from "@/components/reuseableComponents/FormFooter";
@@ -17,9 +16,9 @@ import useTogglePasswordVisibility from "@/hooks/togglePasswordVisibility";
 
 import useReuseableStyles from "@/styles/reuable.styles";
 
-import { auth } from "@/firebase/firebase.config";
+import RegisterLoginButton from "@/components/reuseableComponents/RegisterLoginButton";
+import handleLogin from "@/firebase/users/handleLogin";
 import useVerificationStore from "@/store/verificatonStore";
-import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function LoginScreen() {
   const router = useRouter();
@@ -37,61 +36,8 @@ export default function LoginScreen() {
   // Redirect to verification screen if verification token is not present
   if (!verificationToken) return <Redirect href="/(public)/(auth)" />;
 
-  const isFormValidated = (): boolean => {
-    if (!emailInputRef.current || !passwordInputRef.current) {
-      setError("All fields are required");
-      return false;
-    }
-
-    setError("");
-    return true;
-  };
-
-  const signInUser = async () => {
-    setIsLoading(true);
-    setError("");
-    try {
-      // Create user with email and password
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        emailInputRef.current,
-        passwordInputRef.current
-      );
-      const user = userCredential.user;
-      console.log("Document written with ID: ", user.uid);
-      // On success, you might want to navigate to another screen
-      // For example: router.replace('/(private)/(tabs)');
-    } catch (error: any) {
-      switch (error.code) {
-        case "auth/invalid-credential":
-        case "auth/user-not-found":
-        case "auth/wrong-password":
-          setError("Invalid email or password. Please try again.");
-          break;
-        case "auth/invalid-email":
-          setError("Invalid email address");
-          break;
-        case "auth/too-many-requests":
-          setError("Too many requests");
-          break;
-        case "auth/network-request-failed":
-          setError("Network request failed");
-          break;
-        case "auth/internal-error":
-          setError("Internal error");
-          break;
-        default:
-          setError("Something went wrong");
-          break;
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   const handleSignIn = async () => {
-    if (!isFormValidated()) return;
-    await signInUser();
+    await handleLogin(emailInputRef.current, passwordInputRef.current, setError, setIsLoading);
   };
 
   const navigateToRegister = () => {
@@ -129,9 +75,7 @@ export default function LoginScreen() {
             />
           </View>
 
-          <TouchableOpacity onPress={handleSignIn}>
-            <CustomButton text={"Sign In"} />
-          </TouchableOpacity>
+          <RegisterLoginButton text={"Sign In"} handleSignUp={handleSignIn} />
 
           <FormFooter
             footerText1={"Don't have an account?"}
