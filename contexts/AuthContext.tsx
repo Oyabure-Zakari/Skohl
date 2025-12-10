3606a9f37c8b5a1d4bf0abed562a24b9b5598266
Oyabure Zakari <oyabure205@gmail.com>
2025-12-10 20:49:21 +0100

    create auth provider using firebase onAuthChange to listen for the user authentication state

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,43 @@
+// src/context/AuthContext.tsx
+import { auth } from "@/firebase/firebase.config";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+
+type AuthContextType = {
+  userUid: string | null;
+  user: User | null;
+  loading: boolean;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
+      setLoading(false);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
+  const value = {
+    user,
+    userUid: user?.uid || null,
+    loading,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
