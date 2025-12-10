a8e90e606cd6f2645f17840e491ebc9f7105d080
Oyabure Zakari <oyabure205@gmail.com>
2025-12-10 20:52:11 +0100

    add button to logout user

--- app/(private)/(tabs)/Profile.tsx
+++ app/(private)/(tabs)/Profile.tsx
@@ -1,10 +1,26 @@
+import CustomButton from "@/components/reuseableComponents/CustomButton";
 import COLORS from "@/constants/colors";
-import { StyleSheet, Text, View } from "react-native";
+import { auth } from "@/firebase/firebase.config";
+import useVerificationStore from "@/store/verificatonStore";
+import { signOut } from "firebase/auth";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function ProfileScreen() {
+  const clearToken = useVerificationStore((state) => state.clearVerificationToken);
+  const handleLogOut = async () => {
+    try {
+      await clearToken();
+      await signOut(auth);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
   return (
     <View style={styles.container}>
       <Text>Profile Screen</Text>
+      <TouchableOpacity onPress={handleLogOut}>
+        <CustomButton text={"Log Out"} />
+      </TouchableOpacity>
     </View>
   );
 }
