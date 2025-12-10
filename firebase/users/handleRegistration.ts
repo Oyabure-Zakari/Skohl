f1ab7a35e5fea7c30242ca4a66e2386b7d55fd4d
Oyabure Zakari <oyabure205@gmail.com>
2025-12-10 20:58:48 +0100

    add default image incase uploadedImageUrl is null

--- firebase/users/handleRegistration.ts
+++ firebase/users/handleRegistration.ts
@@ -4,29 +4,25 @@ import isFormFilled from "@/utils/regiserAndLoginFormValidation";
 import createUser from "./createUser";
 import signUpUser from "./signUp";
 
+const defaultImage = "https://res.cloudinary.com/dngo9kz1b/image/upload/v1765389175/qzg6bcjcwsryd70y4qtp.jpg";
+
 const handleRegistration = async (
-image: string,
+  image: string,
   studentInfo: StudentInfoType,
-  emailInputRef: string,  
+  emailInputRef: string,
   passwordInputRef: string,
   confirmPasswordInputRef: string,
   setError: (error: string) => void,
-  setIsLoading: (isLoading: boolean) => void,
+  setIsLoading: (isLoading: boolean) => void
 ) => {
-  if (!isFormFilled(
-      emailInputRef,              
-      passwordInputRef,           
-      setError,                   
-      image,                      
-      confirmPasswordInputRef     
-    ))
+  if (!isFormFilled(emailInputRef, passwordInputRef, setError, image, confirmPasswordInputRef))
     return;
   try {
     setError("");
     setIsLoading(true);
     const uid = await signUpUser(emailInputRef, passwordInputRef, setError);
     const uploadedImageUrl = await generateImageUrl(image, setError);
-    await createUser(uid!, uploadedImageUrl!, studentInfo, setError);
+    await createUser(uid!, uploadedImageUrl ?? defaultImage, studentInfo, setError);
   } catch (error: any) {
     setError(error.message);
   } finally {
