d4cdb4a15845c74cc9050bd95a9986f467a029ec
Oyabure Zakari <oyabure205@gmail.com>
2025-12-13 00:39:16 +0100

    implement bottom sheet

--- app/(private)/(tabs)/index.tsx
+++ app/(private)/(tabs)/index.tsx
@@ -1,13 +1,88 @@
 import FloatingActionButton from "@/components/reuseableComponents/FloatingActionButton";
 import COLORS from "@/constants/colors";
-import { StyleSheet, Text, View } from "react-native";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { StyleSheet, Text } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function ProductsScreen() {
+  const [actionType, setActionType] = useState("");
+
+  const sheetRef = useRef<BottomSheet>(null);
+
+  const snapPoints = useMemo(() => ["1%", "50%", "90%"], []);
+
+  useEffect(() => {
+    if (snapPoints[0] === "1%") {
+      sheetRef.current?.forceClose();
+    }
+  }, [snapPoints]);
+
+  const handleSnapPress = useCallback((index: number) => {
+    sheetRef.current?.snapToIndex(index);
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <Text>Products Screen</Text>
-      <FloatingActionButton />
-    </View>
+      {actionType === "Send Feedback " ? (
+        <BottomSheet
+          ref={sheetRef}
+          snapPoints={snapPoints}
+          enableDynamicSizing={false}
+          backgroundStyle={{
+            borderRadius: 10,
+            backgroundColor: COLORS.white,
+            shadowColor: COLORS.darkBlue,
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 20,
+          }}
+        >
+          <BottomSheetView
+            style={{
+              flex: 1,
+              alignItems: "center",
+            }}
+          >
+            <Text>{actionType}</Text>
+          </BottomSheetView>
+        </BottomSheet>
+      ) : (
+        <BottomSheet
+          ref={sheetRef}
+          snapPoints={snapPoints}
+          enableDynamicSizing={false}
+          backgroundStyle={{
+            borderRadius: 10,
+            backgroundColor: COLORS.white,
+            shadowColor: COLORS.darkBlue,
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 20,
+          }}
+        >
+          <BottomSheetView
+            style={{
+              flex: 1,
+              alignItems: "center",
+            }}
+          >
+            <Text>{actionType}</Text>
+          </BottomSheetView>
+        </BottomSheet>
+      )}
+
+      <FloatingActionButton setActionType={setActionType} handleSnapPress={handleSnapPress} />
+    </GestureHandlerRootView>
   );
 }
 
