0acb4225c639148c60a9ed0fa5404238dbd130ee
Oyabure Zakari <oyabure205@gmail.com>
2025-12-12 12:35:10 +0100

    Add deep link to device settings when image picker permission is denied

--- hooks/expoImagePicker.ts
+++ hooks/expoImagePicker.ts
@@ -1,15 +1,18 @@
 import * as ImagePicker from "expo-image-picker";
+import * as Linking from "expo-linking";
 import { useState } from "react";
 import { Alert } from "react-native";
 
 const useExpoImagePicker = () => {
   const [image, setImage] = useState("");
 
-    const pickImage = async () => {
+  const pickImage = async () => {
     const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
     if (!permissionResult.granted) {
-      Alert.alert("Permission required", "Permission to access the media library is required.");
+      Alert.alert("Permission required", "Permission to access the media library is required.", [
+        { text: "Go to Settings and grant permission", onPress: () => Linking.openSettings() },
+      ]);
       return;
     }
 
@@ -25,7 +28,7 @@ const useExpoImagePicker = () => {
     }
   };
 
-  return { image, pickImage }
+  return { image, pickImage };
 };
 
 export default useExpoImagePicker;
