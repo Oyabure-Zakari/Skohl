7ad2a6a3d0486d01e6d75255f5be2755a69ce0f3
Oyabure Zakari <oyabure205@gmail.com>
2025-12-18 17:23:13 +0100

    Create styles file for gesture handler root view

---
+++ styles/gestureHandlerRootView.styles.ts
@@ -0,0 +1,12 @@
+import COLORS from "@/constants/colors";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    gestureHandlerRootViewContainer: {
+        flex: 1,
+        backgroundColor: COLORS.white,
+        justifyContent: "center",
+        alignItems: "center",
+      }
+  });
+export default styles;
