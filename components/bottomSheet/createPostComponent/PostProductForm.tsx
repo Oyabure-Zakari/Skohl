63a35a07b6f5f7fa5a9511350ea0ee1e3a7cd46b
Oyabure Zakari <oyabure205@gmail.com>
2025-12-25 23:51:11 +0100

    Implement post product functionality in PostProduct component to Firestore

--- components/bottomSheet/createPostComponent/PostProductForm.tsx
+++ components/bottomSheet/createPostComponent/PostProductForm.tsx
@@ -14,7 +14,22 @@ import useCreatePostBottomSheetStyles from "@/styles/createPostBottomSheetStyles
 // Packages
 import { BottomSheetTextInput } from "@gorhom/bottom-sheet";
 // Zustand
+import FormErrorText from "@/components/reuseableComponents/FormErrorText";
+import OverlayLoadingIndicator from "@/components/reuseableComponents/OverlayLoadingIndicator";
+import { useAuth } from "@/contexts/AuthContext";
+import usersCollectionRef from "@/firebase/collectionRef/usersCollectionRef";
+import { db } from "@/firebase/firebase.config";
 import usePhotoStore from "@/store/photoStore";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  query,
+  serverTimestamp,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 
 type PostProductFormProps = {
   postType: "Post a Product" | "Post a Service" | "Post an Event";
@@ -28,10 +43,14 @@ const PostProductForm: React.FC<PostProductFormProps> = ({ postType }) => {
   // States
   const [error, setError] = useState("");
   const [isCameraOpen, setIsCameraOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedProductCategory, setSelectedProductCategory] = useState("");
 
   // Styles
   const createPostStyles = useCreatePostBottomSheetStyles();
+
+  // Current user uid from auth context
+  const { userUid } = useAuth();
 
   // Zustand
   const photo = usePhotoStore((state) => state.image);
@@ -61,15 +80,138 @@ const PostProductForm: React.FC<PostProductFormProps> = ({ postType }) => {
     return true;
   };
 
-  const handlePostProduct = () => {
+  const generateImageUrl = async (): Promise<string | null> => {
+    if (!photo) {
+      setError("No image selected");
+      console.log("Cloudinary: No image selected");
+
+      return null;
+    }
+
+    const detectImageType = () => {
+      if (photo.toLowerCase().endsWith(".jpg") || photo.toLowerCase().endsWith(".jpeg"))
+        return "jpeg";
+      else if (photo.toLowerCase().endsWith(".png")) return "png";
+    };
+
+    const imageType = detectImageType();
+    if (!imageType) {
+      setError("Unsupported image format");
+      console.log("Cloudinary: Unsupported image format");
+      return null;
+    }
+
+    const detectExtension = () => {
+      if (photo.toLowerCase().endsWith(".jpg") || photo.toLowerCase().endsWith(".jpeg"))
+        return "jpeg";
+      else if (photo.toLowerCase().endsWith(".png")) return "png";
+    };
+
+    const extension = detectExtension();
+    if (!extension) {
+      setError("Extension not found");
+      console.log("Cloudinary: Extension not found");
+      return null;
+    }
+
+    try {
+      const formData = new FormData();
+      formData.append("file", {
+        uri: photo,
+        type: `image/${imageType}`,
+        name: `upload.${extension}`,
+      } as any);
+      formData.append("upload_preset", process.env.EXPO_PUBLIC_CLOUDINARY_UPLOADPRESET!);
+      formData.append("cloud_name", process.env.EXPO_PUBLIC_CLOUDINARY_CLOUDNAME!);
+
+      const response = await fetch(
+        `https://api.cloudinary.com/v1_1/${process.env.EXPO_PUBLIC_CLOUDINARY_CLOUDNAME}/image/upload`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        setError(`Upload failed: ${response.status} ${errorText}`);
+        console.log(`Cloudinary: Upload failed: ${response.status} ${errorText}`);
+        return null;
+      }
+
+      const data = await response.json();
+      console.log("Cloudinary Image uploaded");
+      return data.secure_url || null;
+    } catch (err: any) {
+      setError(`Network error: ${err.message}`);
+      console.log(`Cloudibary Network error: ${err.message}`);
+      return null;
+    }
+  };
+
+  const postProduct = async () => {
+    try {
+      // Variables to hold user's info
+      let fullName;
+      let image;
+
+      // Query user document
+      const q = query(usersCollectionRef, where("uid", "==", userUid));
+      const snapshot = await getDocs(q);
+
+      // Get user's info
+      snapshot.forEach((doc) => {
+        const data = doc.data();
+        fullName = `${data.surname} ${data.firstname}`;
+        image = data.image;
+      });
+
+      // Upload image to cloudinary and get URL
+      const uploadedImage = await generateImageUrl();
+      console.log("Uploaded Image:", uploadedImage);
+
+      // Add document
+      const docRef = await addDoc(collection(db, "products"), {
+        id: "",
+        name: productNameRef.current.trim(),
+        price: productPriceRef.current.trim(),
+        description: productDescriptionRef.current.trim(),
+        category: selectedProductCategory.trim(),
+        photo: uploadedImage,
+        postedBy: { userUid, fullName, image },
+        createdAt: serverTimestamp(),
+      });
+
+      console.log("Firebase: product added successfully");
+
+      // Update document with its own ID
+      await updateDoc(doc(db, "products", docRef.id), { id: docRef.id });
+    } catch (error: any) {
+      setError(error.message);
+      console.log("Firebase: ", error.message);
+    }
+  };
+  const handlePostProduct = async () => {
     if (isProductFormValid()) {
-      console.log("Product posted!");
-      // After posting either failed or successful, reset, photo, and form fields
+      try {
+        setIsLoading(true);
+        await postProduct();
+        console.log("Product posted!");
+      } catch (error: any) {
+        setError(error.message);
+        console.log(error.message);
+      } finally {
+        setIsLoading(false);
+      }
     }
   };
 
   if (isCameraOpen) {
     return <DeviceCamera setIsCameraOpen={setIsCameraOpen} />;
+  }
+
+  if (isLoading) {
+    return <OverlayLoadingIndicator />;
   }
 
   return (
@@ -83,8 +225,9 @@ const PostProductForm: React.FC<PostProductFormProps> = ({ postType }) => {
 
       {/* Form Section */}
       <View style={createPostStyles.formContainer}>
+        <FormErrorText error={error} />
         <BottomSheetTextInput
-          placeholder="Post Name"
+          placeholder="Product Name"
           onChangeText={(text) => {
             productNameRef.current = text;
             if (error) setError("");
