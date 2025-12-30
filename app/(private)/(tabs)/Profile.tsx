7594d7c77d0da6264d9d6324cb590b0b29f426aa
Oyabure Zakari <oyabure205@gmail.com>
2025-12-30 10:34:42 +0100

    Add bottom sheet UI logic in Profile screen

--- app/(private)/(tabs)/Profile.tsx
+++ app/(private)/(tabs)/Profile.tsx
@@ -1,12 +1,25 @@
+import BottomSheetComponent from "@/components/bottomSheet/BottomSheetComponent";
 import CustomButton from "@/components/reuseableComponents/CustomButton";
 import FloatingActionButton from "@/components/reuseableComponents/FloatingActionButton";
 import COLORS from "@/constants/colors";
 import { auth } from "@/firebase/firebase.config";
 import useVerificationStore from "@/store/verificatonStore";
+import gestureHandlerRootViewStyle from "@/styles/gestureHandlerRootView.styles";
+import BottomSheet from "@gorhom/bottom-sheet";
 import { signOut } from "firebase/auth";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useCallback, useMemo, useRef, useState } from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function ProfileScreen() {
+  // States
+  const [activeBottomSheet, setActiveBottomSheet] = useState<"Create Post" | "Send Feedback">(
+    "Create Post"
+  );
+
+  // Refs
+  const sheetRef = useRef<BottomSheet>(null);
+
   const clearToken = useVerificationStore((state) => state.clearVerificationToken);
   const handleLogOut = async () => {
     try {
@@ -16,14 +29,31 @@ export default function ProfileScreen() {
       console.log(error.message);
     }
   };
+
+  const snapPoints = useMemo(() => ["1%", "50%", "100%"], []);
+
+  const handleSnapPress = useCallback(() => {
+    sheetRef.current?.snapToIndex(2);
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={gestureHandlerRootViewStyle.container}>
       <Text>Profile Screen</Text>
       <TouchableOpacity onPress={handleLogOut}>
         <CustomButton text={"Log Out"} />
       </TouchableOpacity>
-      <FloatingActionButton />
-    </View>
+
+      <BottomSheetComponent
+        sheetRef={sheetRef}
+        snapPoints={snapPoints}
+        activeBottomSheet={activeBottomSheet}
+      />
+
+      <FloatingActionButton
+        setActiveBottomSheet={setActiveBottomSheet}
+        handleSnapPress={handleSnapPress}
+      />
+    </GestureHandlerRootView>
   );
 }
 
