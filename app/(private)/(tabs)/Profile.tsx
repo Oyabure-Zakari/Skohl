d590d94278578b87767fbd50a9863868e1e9b213
Oyabure Zakari <oyabure205@gmail.com>
2026-01-01 21:25:55 +0100

    Format month better in profile screen

--- app/(private)/(tabs)/Profile.tsx
+++ app/(private)/(tabs)/Profile.tsx
@@ -87,46 +87,15 @@ export default function ProfileScreen() {
 
   // Your timestamp object from Firestore
   const firestoreTimestamp = {
-    seconds: user.joinedAt.seconds,
+    seconds: user.joinedAt.seconds, // seconds should always come first
     nanoseconds: user.joinedAt.nanoseconds,
   };
 
   // Convert to Firebase Timestamp, then to JavaScript Date
   const date = new Timestamp(firestoreTimestamp.seconds, firestoreTimestamp.nanoseconds).toDate();
 
-  const year = date.getFullYear(); // 2025
-  const month = date.getMonth() + 1; // 12 (December) — +1 because months are 0-indexed
-
-  const formatDate = () => {
-    switch (month) {
-      case 1:
-        return `January, ${year}`;
-      case 2:
-        return `February, ${year}`;
-      case 3:
-        return `March, ${year}`;
-      case 4:
-        return `April, ${year}`;
-      case 5:
-        return `May, ${year}`;
-      case 6:
-        return `June, ${year}`;
-      case 7:
-        return `July, ${year}`;
-      case 8:
-        return `August, ${year}`;
-      case 9:
-        return `September, ${year}`;
-      case 10:
-        return `October, ${year}`;
-      case 11:
-        return `November, ${year}`;
-      case 12:
-        return `December, ${year}`;
-      default:
-        return "Invalid month";
-    }
-  };
+  const year = date.getFullYear();
+  const month = date.toLocaleString("default", { month: "long" });
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
@@ -173,7 +142,9 @@ export default function ProfileScreen() {
         {/* Joined Date */}
         <View style={{ flexDirection: "row", alignItems: "center" }}>
           <MaterialIcons name="date-range" size={20} color={COLORS.darkGrey} />
-          <Text style={profileStyles.bioText2}> {formatDate() && `Joined ${formatDate()}`} </Text>
+          <Text style={profileStyles.bioText2}>
+            Joined {month}, {year}
+          </Text>
         </View>
 
         {/* Display bio if available */}
