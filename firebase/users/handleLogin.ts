996aed6dbf611d79f59ba133e37688280f46ff29
Oyabure Zakari <oyabure205@gmail.com>
2026-01-04 19:39:39 +0100

    Pass verificationFingerprint to the signInUser function in handleLogin file

--- firebase/users/handleLogin.ts
+++ firebase/users/handleLogin.ts
@@ -7,16 +7,18 @@ const handleLogin = async (
   passwordInputRef: React.RefObject<string>,
   textInputRef: React.RefObject<TextInput | null>,
   setError: (error: string) => void,
-  setIsLoading: (isLoading: boolean) => void
+  setIsLoading: (isLoading: boolean) => void,
+  verificationFingerprint: string
 ) => {
   // Define the current screen for validation
   const authScreen = "Login Screen";
+
   // Validate form inputs
   if (!isFormFilled(emailInputRef.current, passwordInputRef.current, setError, authScreen)) return;
   try {
     setIsLoading(true);
     // Attempt to sign in the user
-    await signInUser(emailInputRef.current, passwordInputRef.current, setError);
+    await signInUser(emailInputRef.current, passwordInputRef.current, setError, verificationFingerprint);
   } catch (error: any) {
     setError(error.message);
   } finally {
