a74fde369d2934d7ee7f91e40c1df64e3e2dac8a
Oyabure Zakari <oyabure205@gmail.com>
2026-01-13 21:27:43 +0100

    Edit expo status bar style in the Camera component file

--- components/bottomSheet/createPostComponent/Camera.tsx
+++ components/bottomSheet/createPostComponent/Camera.tsx
@@ -3,6 +3,7 @@ import usePhotoStore from "@/store/photoStore";
 import cameraStyles from "@/styles/camera.styles";
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
+import { StatusBar } from "expo-status-bar";
 import { Dispatch, SetStateAction, useRef, useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 // Import the menu
@@ -52,99 +53,102 @@ const DeviceCamera: React.FC<DeviceCameraProps> = ({ setIsCameraOpen }) => {
   };
 
   return (
-    <View style={cameraStyles.cameraContainer}>
-      <CameraView
-        style={cameraStyles.camera}
-        ref={cameraRef}
-        mode={"picture"}
-        facing={facing}
-        flash={flashMode}
-        ratio={"1:1"}
-        responsiveOrientationWhenOrientationLocked
-      />
+    <>
+      <StatusBar style="light" backgroundColor="black" />
+      <View style={cameraStyles.cameraContainer}>
+        <CameraView
+          style={cameraStyles.camera}
+          ref={cameraRef}
+          mode={"picture"}
+          facing={facing}
+          flash={flashMode}
+          ratio={"1:1"}
+          responsiveOrientationWhenOrientationLocked
+        />
 
-      <TouchableOpacity onPress={() => setIsMenuVisible(true)} style={{ padding: 16 }}>
-        {flashMode === "auto" && (
-          <MaterialCommunityIcons name="flash-auto" size={24} color="white" />
-        )}
-        {flashMode === "off" && (
-          <MaterialCommunityIcons name="flash-off-outline" size={24} color="white" />
-        )}
-        {flashMode === "on" && <MaterialCommunityIcons name="flash" size={24} color="white" />}
-      </TouchableOpacity>
-
-      <Menu visible={isMenuVisible} onRequestClose={() => setIsMenuVisible(false)}>
-        <MenuItem>
-          <TouchableOpacity
-            onPress={() => {
-              setFlashMode("auto");
-              setIsMenuVisible(false);
-            }}
-            style={{ flexDirection: "row", alignItems: "center" }}
-          >
-            <MaterialCommunityIcons name="flash-auto" size={24} color="black" />
-            <Text> Auto</Text>
-          </TouchableOpacity>
-        </MenuItem>
-
-        <MenuDivider />
-
-        <MenuItem>
-          <TouchableOpacity
-            onPress={() => {
-              setFlashMode("off");
-              setIsMenuVisible(false);
-            }}
-            style={{ flexDirection: "row", alignItems: "center" }}
-          >
-            <MaterialCommunityIcons name="flash-off-outline" size={24} color="black" />
-            <Text>Flash Off</Text>
-          </TouchableOpacity>
-        </MenuItem>
-
-        <MenuDivider />
-
-        <MenuItem>
-          <TouchableOpacity
-            onPress={() => {
-              setFlashMode("on");
-              setIsMenuVisible(false);
-            }}
-            style={{ flexDirection: "row", alignItems: "center" }}
-          >
-            <MaterialCommunityIcons name="flash" size={24} color="black" />
-            <Text>Flash On</Text>
-          </TouchableOpacity>
-        </MenuItem>
-      </Menu>
-
-      {/* Shutter Container */}
-      <View style={cameraStyles.shutterContainer}>
-        {/* Close Camera */}
-        <TouchableOpacity onPress={closeCamera}>
-          <MaterialCommunityIcons name="camera-off" size={30} color="white" />
+        <TouchableOpacity onPress={() => setIsMenuVisible(true)} style={{ padding: 16 }}>
+          {flashMode === "auto" && (
+            <MaterialCommunityIcons name="flash-auto" size={24} color="white" />
+          )}
+          {flashMode === "off" && (
+            <MaterialCommunityIcons name="flash-off-outline" size={24} color="white" />
+          )}
+          {flashMode === "on" && <MaterialCommunityIcons name="flash" size={24} color="white" />}
         </TouchableOpacity>
 
-        {/* Shutter Button */}
-        <TouchableOpacity onPress={takePicture}>
-          <View style={cameraStyles.shutterBtn}>
-            <View
-              style={[
-                cameraStyles.shutterBtnInner,
-                {
-                  backgroundColor: "white",
-                },
-              ]}
-            />
-          </View>
-        </TouchableOpacity>
+        <Menu visible={isMenuVisible} onRequestClose={() => setIsMenuVisible(false)}>
+          <MenuItem>
+            <TouchableOpacity
+              onPress={() => {
+                setFlashMode("auto");
+                setIsMenuVisible(false);
+              }}
+              style={{ flexDirection: "row", alignItems: "center" }}
+            >
+              <MaterialCommunityIcons name="flash-auto" size={24} color="black" />
+              <Text> Auto</Text>
+            </TouchableOpacity>
+          </MenuItem>
 
-        {/* Switch Camera */}
-        <TouchableOpacity onPress={toggleFacing}>
-          <MaterialIcons name="cameraswitch" size={30} color="white" />
-        </TouchableOpacity>
+          <MenuDivider />
+
+          <MenuItem>
+            <TouchableOpacity
+              onPress={() => {
+                setFlashMode("off");
+                setIsMenuVisible(false);
+              }}
+              style={{ flexDirection: "row", alignItems: "center" }}
+            >
+              <MaterialCommunityIcons name="flash-off-outline" size={24} color="black" />
+              <Text>Flash Off</Text>
+            </TouchableOpacity>
+          </MenuItem>
+
+          <MenuDivider />
+
+          <MenuItem>
+            <TouchableOpacity
+              onPress={() => {
+                setFlashMode("on");
+                setIsMenuVisible(false);
+              }}
+              style={{ flexDirection: "row", alignItems: "center" }}
+            >
+              <MaterialCommunityIcons name="flash" size={24} color="black" />
+              <Text>Flash On</Text>
+            </TouchableOpacity>
+          </MenuItem>
+        </Menu>
+
+        {/* Shutter Container */}
+        <View style={cameraStyles.shutterContainer}>
+          {/* Close Camera */}
+          <TouchableOpacity onPress={closeCamera}>
+            <MaterialCommunityIcons name="camera-off" size={30} color="white" />
+          </TouchableOpacity>
+
+          {/* Shutter Button */}
+          <TouchableOpacity onPress={takePicture}>
+            <View style={cameraStyles.shutterBtn}>
+              <View
+                style={[
+                  cameraStyles.shutterBtnInner,
+                  {
+                    backgroundColor: "white",
+                  },
+                ]}
+              />
+            </View>
+          </TouchableOpacity>
+
+          {/* Switch Camera */}
+          <TouchableOpacity onPress={toggleFacing}>
+            <MaterialIcons name="cameraswitch" size={30} color="white" />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </>
   );
 };
 
