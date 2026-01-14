f70125e6576125fd06d6d6ddd78a537ebe9f8ed8
Oyabure Zakari <oyabure205@gmail.com>
2026-01-14 22:19:19 +0100

    Break down camera components into their own seperate sub components in Camera component

--- components/bottomSheet/createPostComponent/Camera.tsx
+++ components/bottomSheet/createPostComponent/Camera.tsx
@@ -1,17 +1,17 @@
 // React
 import { Dispatch, SetStateAction, useRef, useState } from "react";
 // React Native
-import { Text, TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
 // Expo
-import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
 import { StatusBar } from "expo-status-bar";
 // Packages/Libraries
-import { Menu, MenuDivider, MenuItem } from "react-native-material-menu";
+import { Menu } from "react-native-material-menu";
 // Components
-import CustomButton from "@/components/reuseableComponents/CustomButton";
-// Zustand
-import usePhotoStore from "@/store/photoStore";
+import FlashModeButton from "./cameraComponent/FlashModeButton";
+import FlashModes from "./cameraComponent/FlashModes";
+import GrantCameraPermission from "./cameraComponent/GrantCameraPermission";
+import ShutterContents from "./cameraComponent/ShutterContents";
 // Styles
 import cameraStyles from "@/styles/camera.styles";
 
@@ -31,9 +31,6 @@ const DeviceCamera: React.FC<DeviceCameraProps> = ({ setIsCameraOpen }) => {
   // Expo Camera Hooks
   const [permission, requestPermission] = useCameraPermissions();
 
-  // Zustand
-  const setImage = usePhotoStore((state) => state.setImage);
-
   // If no permission
   if (!permission) {
     return null;
@@ -41,36 +38,14 @@ const DeviceCamera: React.FC<DeviceCameraProps> = ({ setIsCameraOpen }) => {
 
   // If permission is not granted
   if (!permission.granted) {
-    return (
-      <View style={cameraStyles.grantPermissionContainer}>
-        <Text style={{ textAlign: "center" }}>We need your permission to use the camera</Text>
-        <TouchableOpacity onPress={requestPermission}>
-          <CustomButton text={"Grant Permission"} />
-        </TouchableOpacity>
-      </View>
-    );
+    return <GrantCameraPermission requestPermission={requestPermission} />;
   }
-
-  const closeCamera = () => {
-    setIsCameraOpen(false);
-  };
-
-  const takePicture = async () => {
-    const photo = await cameraRef.current?.takePictureAsync();
-    if (photo?.uri) {
-      setImage(photo.uri);
-      setIsCameraOpen(false);
-    }
-  };
-
-  const toggleFacing = () => {
-    setFacing((prev) => (prev === "back" ? "front" : "back"));
-  };
 
   return (
     <>
       <StatusBar style="light" backgroundColor="black" />
       <View style={cameraStyles.cameraContainer}>
+        {/* Camera */}
         <CameraView
           style={cameraStyles.camera}
           ref={cameraRef}
@@ -81,92 +56,20 @@ const DeviceCamera: React.FC<DeviceCameraProps> = ({ setIsCameraOpen }) => {
           responsiveOrientationWhenOrientationLocked
         />
 
-        {/* Opens the menu reguardless of the flash mode that is set */}
-        <TouchableOpacity onPress={() => setIsMenuVisible(true)} style={{ padding: 16 }}>
-          {flashMode === "auto" && (
-            <MaterialCommunityIcons name="flash-auto" size={24} color="white" />
-          )}
-          {flashMode === "off" && (
-            <MaterialCommunityIcons name="flash-off-outline" size={24} color="white" />
-          )}
-          {flashMode === "on" && <MaterialCommunityIcons name="flash" size={24} color="white" />}
-        </TouchableOpacity>
+        {/* Opens the menu reguardless of which flash mode is selected */}
+        <FlashModeButton setIsMenuVisible={setIsMenuVisible} flashMode={flashMode} />
 
         {/* Flash Mode Menu */}
         <Menu visible={isMenuVisible} onRequestClose={() => setIsMenuVisible(false)}>
-          {/* Flash Auto */}
-          <MenuItem>
-            <TouchableOpacity
-              onPress={() => {
-                setFlashMode("auto");
-                setIsMenuVisible(false);
-              }}
-              style={{ flexDirection: "row", alignItems: "center" }}
-            >
-              <MaterialCommunityIcons name="flash-auto" size={24} color="black" />
-              <Text> Auto</Text>
-            </TouchableOpacity>
-          </MenuItem>
-
-          <MenuDivider />
-
-          {/* Flash Off */}
-          <MenuItem>
-            <TouchableOpacity
-              onPress={() => {
-                setFlashMode("off");
-                setIsMenuVisible(false);
-              }}
-              style={{ flexDirection: "row", alignItems: "center" }}
-            >
-              <MaterialCommunityIcons name="flash-off-outline" size={24} color="black" />
-              <Text>Flash Off</Text>
-            </TouchableOpacity>
-          </MenuItem>
-
-          <MenuDivider />
-
-          {/* Flash On */}
-          <MenuItem>
-            <TouchableOpacity
-              onPress={() => {
-                setFlashMode("on");
-                setIsMenuVisible(false);
-              }}
-              style={{ flexDirection: "row", alignItems: "center" }}
-            >
-              <MaterialCommunityIcons name="flash" size={24} color="black" />
-              <Text>Flash On</Text>
-            </TouchableOpacity>
-          </MenuItem>
+          <FlashModes setFlashMode={setFlashMode} setIsMenuVisible={setIsMenuVisible} />
         </Menu>
 
         {/* Shutter Container */}
-        <View style={cameraStyles.shutterContainer}>
-          {/* Close Camera */}
-          <TouchableOpacity onPress={closeCamera}>
-            <MaterialCommunityIcons name="camera-off" size={30} color="white" />
-          </TouchableOpacity>
-
-          {/* Take Picture */}
-          <TouchableOpacity onPress={takePicture}>
-            <View style={cameraStyles.shutterBtn}>
-              <View
-                style={[
-                  cameraStyles.shutterBtnInner,
-                  {
-                    backgroundColor: "white",
-                  },
-                ]}
-              />
-            </View>
-          </TouchableOpacity>
-
-          {/* Switch Camera */}
-          <TouchableOpacity onPress={toggleFacing}>
-            <MaterialIcons name="cameraswitch" size={30} color="white" />
-          </TouchableOpacity>
-        </View>
+        <ShutterContents
+          cameraRef={cameraRef}
+          setFacing={setFacing}
+          setIsCameraOpen={setIsCameraOpen}
+        />
       </View>
     </>
   );
