86fda8f8c9673d3f602150a9232fa651a4421d24
Oyabure Zakari <oyabure205@gmail.com>
2026-01-20 17:45:35 +0100

    Define posts interface in Profile screen

--- app/(private)/(tabs)/Profile.tsx
+++ app/(private)/(tabs)/Profile.tsx
@@ -21,6 +21,48 @@ import postsCollectionRef from "@/firebase/collectionRef/postsCollectionRef";
 import { useUserProfile } from "@/hooks/userProfile";
 import { getDocs, orderBy, query, where } from "firebase/firestore";
 
+type PostType = "event" | "service" | "product";
+
+interface BasePost {
+  id: string;
+  title: string;
+  category: string;
+  description: string;
+  postType: PostType;
+  createdAt: { seconds: number; nanoseconds: number; type: string };
+  photo?: string; // optional
+  postedBy: {
+    fullName: string;
+    image?: string;
+    userUid: string;
+  };
+}
+
+// Event-specific fields
+interface EventPost extends BasePost {
+  postType: "event";
+  eventDate: string;
+  eventTime?: string;
+  eventType?: string;
+  eventVenue?: string;
+}
+
+// Service-specific fields
+interface ServicePost extends BasePost {
+  postType: "service";
+  price?: string;
+  serviceSchedule?: string;
+}
+
+// Product-specific fields
+interface ProductPost extends BasePost {
+  postType: "product";
+  price?: string;
+}
+
+// Union type for all posts
+type Post = EventPost | ServicePost | ProductPost;
+
 export default function ProfileScreen() {
   // Currently logged in user
   const { userUid } = useAuth();
@@ -50,6 +92,7 @@ export default function ProfileScreen() {
 
   // Fetch created by the user
   const [isLoadingCreatedPosts, setIsLoadingCreatedPosts] = useState(true);
+  const [posts, setPosts] = useState<Post[]>([]);
   const fecthCreatedPosts = async () => {
     try {
       const q = query(
@@ -61,7 +104,7 @@ export default function ProfileScreen() {
 
       snapshot.forEach((doc) => {
         const data = doc.data();
-        console.log(data);
+        setPosts(data as Post[]);
       });
     } catch (error: any) {
       console.log(error.message);
@@ -69,6 +112,8 @@ export default function ProfileScreen() {
       setIsLoadingCreatedPosts(false);
     }
   };
+
+  console.log("Posts", posts);
 
   useEffect(() => {
     fecthCreatedPosts();
