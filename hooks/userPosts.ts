59ed7cfd05c4075b6af014cea5a4849de1c42ebf
Oyabure Zakari <oyabure205@gmail.com>
2026-01-28 13:52:37 +0100

    Use firebase firestore  onSnapshot to for real time update in userPosts hook file

--- hooks/userPosts.ts
+++ hooks/userPosts.ts
@@ -1,7 +1,17 @@
+import postsCollectionRef from "@/firebase/collectionRef/postsCollectionRef";
 import { Post } from "@/types/PostTypes";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { onSnapshot, orderBy, query, where } from "firebase/firestore";
+import { useEffect } from "react";
+import { useWindowDimensions } from "react-native";
+import Toast from "react-native-toast-message";
 
 export const useUserPosts = (userUid: string | null | undefined) => {
+  const { fontScale } = useWindowDimensions();
+
+  //
+  const queryClient = useQueryClient();
+
   const {
     data: posts = [],
     isLoading: isLoadingCreatedPosts,
@@ -14,9 +24,52 @@ export const useUserPosts = (userUid: string | null | undefined) => {
     gcTime: 1000 * 60 * 5, // Keep in cache 5 min after unmount
     queryFn: () => {
       // This is a placeholder — we never actually "fetch" here
-      return Promise.resolve([]); // or throw if you want
+      return Promise.resolve([]); // Its going to return a promise array
     },
   });
+
+  // Real-time listener runs once per userUid change, listens for changes in posts collection (add/update/delete)
+  useEffect(() => {
+    // If no userUid, clear cache
+    if (!userUid) {
+      // Clear cache
+      queryClient.setQueryData(["userPosts", userUid], []);
+      return;
+    }
+
+    // Fetch user's posts
+    const q = query(
+      postsCollectionRef,
+      where("postedBy.userUid", "==", userUid),
+      orderBy("createdAt", "desc"),
+    );
+
+    // Real-time listener, listens for changes in posts collection (add/update/delete)
+    const unsubscribe = onSnapshot(
+      q,
+      (snapshot) => {
+        const fetchedPosts: Post[] = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        })) as Post[];
+
+        // setQueryData will update TanStack Query's cache with the fresh data from Firestore
+        queryClient.setQueryData(["userPosts", userUid], fetchedPosts);
+      },
+      (error) => {
+        console.error("Posts real-time error:", error);
+        Toast.show({
+          type: "error",
+          text1: "Failed to load posts",
+          text2: error.message,
+          text1Style: { fontSize: fontScale * 16, fontFamily: "Segoe_UI_Bold" },
+          text2Style: { fontSize: fontScale * 12, fontFamily: "Segoe_UI_Bold" },
+        });
+      },
+    );
+
+    return () => unsubscribe();
+  }, [userUid]);
 
   return {
     posts,
