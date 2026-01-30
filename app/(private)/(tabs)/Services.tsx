3e044c68ff2e4e20dc230e157b43367afe843f8e
Oyabure Zakari <oyabure205@gmail.com>
2026-01-30 13:15:55 +0100

    Use GestureHandlerRootView and FloatingActionButton in Services and Events tabs screen

--- app/(private)/(tabs)/Services.tsx
+++ app/(private)/(tabs)/Services.tsx
@@ -1,13 +1,46 @@
+import BottomSheetComponent from "@/components/bottomSheet/BottomSheetComponent";
 import FloatingActionButton from "@/components/reuseableComponents/FloatingActionButton";
 import COLORS from "@/constants/colors";
-import { StyleSheet, Text, View } from "react-native";
+import gestureHandlerRootViewStyle from "@/styles/gestureHandlerRootView.styles";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { useCallback, useMemo, useRef, useState } from "react";
+import { StyleSheet, Text } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function ServicesScreen() {
+  // States
+  const [activeBottomSheet, setActiveBottomSheet] = useState<"Create Post" | "Send Feedback">(
+    "Create Post",
+  );
+
+  // Refs
+  const sheetRef = useRef<BottomSheet>(null);
+
+  // Bottom Sheet snap points
+  const snapPoints = useMemo(() => ["1%", "50%", "100%"], []);
+
+  // Handlers
+  const handleSnapPress = useCallback(() => {
+    sheetRef.current?.snapToIndex(2);
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={gestureHandlerRootViewStyle.container}>
       <Text>Services Screen</Text>
-      <FloatingActionButton />
-    </View>
+
+      {/* Bottom Sheet */}
+      <BottomSheetComponent
+        sheetRef={sheetRef}
+        snapPoints={snapPoints}
+        activeBottomSheet={activeBottomSheet}
+      />
+
+      {/* Floating Action Button */}
+      <FloatingActionButton
+        setActiveBottomSheet={setActiveBottomSheet}
+        handleSnapPress={handleSnapPress}
+      />
+    </GestureHandlerRootView>
   );
 }
 
