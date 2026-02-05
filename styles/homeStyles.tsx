77e60d2c5bc7c2b4211eb3a0e0a483dec5b8a7da
Oyabure Zakari <oyabure205@gmail.com>
2026-02-05 18:33:06 +0100

    Create file for home screen styles in homeStyles file

---
+++ styles/homeStyles.tsx
@@ -0,0 +1,38 @@
+import COLORS from "@/constants/colors";
+import { StyleSheet } from "react-native";
+
+export default function useHomeStyles() {
+  return StyleSheet.create({
+    header: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingHorizontal: 16,
+    },
+
+    userName: {
+      fontSize: 18,
+      fontFamily: "Segoe_UI_Bold",
+      color: COLORS.darkBlue,
+    },
+
+    divider: {
+      width: "100%",
+      height: 2,
+      backgroundColor: COLORS.lightGrey,
+      marginTop: 10,
+    },
+
+    categoryContainer: {
+      marginTop: 20,
+      paddingHorizontal: 4,
+    },
+
+    categoryTitle: {
+      fontSize: 18,
+      fontFamily: "Segoe_UI_Bold",
+      color: COLORS.purple,
+      paddingHorizontal: 16,
+      marginBottom: 6,
+    },
+  });
+}
