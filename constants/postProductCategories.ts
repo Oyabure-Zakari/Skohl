8b416eba246e0ca5691ef0100a1a96b04678af73
Oyabure Zakari <oyabure205@gmail.com>
2026-02-05 12:27:00 +0100

    Move postCatergory array to its own file in constant folder

---
+++ constants/postProductCategories.ts
@@ -0,0 +1,15 @@
+const productCategories = [
+  "none",
+  "Books & Academic Materials",
+  "Electronics & Gadgets",
+  "Equipments",
+  "Fashion & Clothing",
+  "Hostel & Room Essentials",
+  "Kitchen & Food Items",
+  "Personal Care & Beauty",
+  "Sportswear",
+  "Stationery & Office Supplies",
+  "Transportation & Mobility",
+];
+
+export default productCategories;
