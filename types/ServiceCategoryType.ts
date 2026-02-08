6830197589388dc314d811e7a7ed4a96b18f0922
Oyabure Zakari <oyabure205@gmail.com>
2026-02-08 07:51:21 +0100

    Create types for event category and service category in EventCategoryType and ServiceCategoryType respectively

---
+++ types/ServiceCategoryType.ts
@@ -0,0 +1,13 @@
+export type ServiceCategoryType =
+  | "none"
+  | "Academic & Educational"
+  | "Advertising & Business Services"
+  | "Beauty & Fashion"
+  | "Creative & Media"
+  | "Events & Catering"
+  | "Errands & Daily Services"
+  | "Lifestyle & Wellness"
+  | "Repairs & Maintenance"
+  | "Tech & Digital Services"
+  | "Transportation & Logistics"
+  | "Virtual & Freelance Services";
