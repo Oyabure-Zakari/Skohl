41ff8764e716dc9644a68b5aec82f4f60df474ba
Oyabure Zakari <oyabure205@gmail.com>
2026-02-15 17:12:05 +0100

    Add touchable opacity to navigate to UserProfilePicture [id] component in PostDetailsUerImage component

--- components/postDetails/PostDetailsUserImage.tsx
+++ components/postDetails/PostDetailsUserImage.tsx
@@ -1,19 +1,25 @@
 import blurhash from "@/constants/expoBlurImage";
 import { Image } from "expo-image";
+import { useRouter } from "expo-router";
+import { TouchableOpacity } from "react-native";
 
 type PostDetailsUserImageProps = {
   postUserImage: string | undefined;
+  userUid: string | undefined;
 };
 
-const PostDetailsUserImage: React.FC<PostDetailsUserImageProps> = ({ postUserImage }) => {
+const PostDetailsUserImage: React.FC<PostDetailsUserImageProps> = ({ postUserImage, userUid }) => {
+  const router = useRouter();
   return (
-    <Image
-      source={{ uri: postUserImage }}
-      style={{ width: 50, height: 50, borderRadius: 25 }}
-      placeholder={{ blurhash }}
-      transition={300}
-      contentFit="cover"
-    />
+    <TouchableOpacity onPress={() => router.push(`/(private)/userProfilePicture/${userUid}`)}>
+      <Image
+        source={{ uri: postUserImage }}
+        style={{ width: 50, height: 50, borderRadius: 25 }}
+        placeholder={{ blurhash }}
+        transition={300}
+        contentFit="cover"
+      />
+    </TouchableOpacity>
   );
 };
 
