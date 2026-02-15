3444370263d238561712e1dd1b349cf4c370e971
Oyabure Zakari <oyabure205@gmail.com>
2026-02-15 17:10:19 +0100

    Add touchable opacity to navigate to UserProfilePicture [id] component in PostUerImage component

--- components/reuseableComponents/postsFeedComponent/postCardVertical/PostUserImage.tsx
+++ components/reuseableComponents/postsFeedComponent/postCardVertical/PostUserImage.tsx
@@ -1,23 +1,29 @@
 import blurhash from "@/constants/expoBlurImage";
 import usePostCardVerticalStyles from "@/styles/postCardVerticalStyles";
 import { Image } from "expo-image";
+import { useRouter } from "expo-router";
 import React from "react";
+import { TouchableOpacity } from "react-native";
 
 type PostUserImageProps = {
   image: string | undefined;
+  userUid: string | undefined;
 };
 
-const PostUserImage: React.FC<PostUserImageProps> = ({ image }) => {
+const PostUserImage: React.FC<PostUserImageProps> = ({ image, userUid }) => {
   const postCardVerticalStyles = usePostCardVerticalStyles();
+  const router = useRouter();
   return (
-    <Image
-      source={{ uri: image }}
-      style={postCardVerticalStyles.userAvatar}
-      placeholder={{ blurhash }}
-      contentFit="contain"
-      transition={1000}
-      alt="Profile Picture"
-    />
+    <TouchableOpacity onPress={() => router.push(`/(private)/userProfilePicture/${userUid}`)}>
+      <Image
+        source={{ uri: image }}
+        style={postCardVerticalStyles.userAvatar}
+        placeholder={{ blurhash }}
+        contentFit="contain"
+        transition={1000}
+        alt="Profile Picture"
+      />
+    </TouchableOpacity>
   );
 };
 
