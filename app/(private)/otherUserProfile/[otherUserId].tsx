7e27211e9caf8e8b1b5272e77bcfc271b073068f
Oyabure Zakari <oyabure205@gmail.com>
2026-02-15 21:28:08 +0100

    Fully design other user profile screen

--- app/(private)/otherUserProfile/[otherUserId].tsx
+++ app/(private)/otherUserProfile/[otherUserId].tsx
@@ -1,22 +1,28 @@
 // React
 import BottomSheetComponent from "@/components/bottomSheet/BottomSheetComponent";
-import Header from "@/components/profile/Header";
 import UserBio from "@/components/profile/UserBio";
 import FloatingActionButton from "@/components/reuseableComponents/FloatingActionButton";
 import PostsList from "@/components/reuseableComponents/postsFeedComponent/PostsList";
+import COLORS from "@/constants/colors";
+import blurhash from "@/constants/expoBlurImage";
 import { useUserPosts } from "@/hooks/userPosts";
 import { useUserProfile } from "@/hooks/userProfile";
 import gestureHandlerRootViewStyle from "@/styles/gestureHandlerRootView.styles";
 import useReuseableStyles from "@/styles/reuable.styles";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import BottomSheet from "@gorhom/bottom-sheet";
-import { useLocalSearchParams } from "expo-router";
+import { Image } from "expo-image";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { ScrollView, View } from "moti";
 import React, { useCallback, useMemo, useRef, useState } from "react";
-import { Alert } from "react-native";
+import { Alert, StyleSheet, Text, TouchableOpacity } from "react-native";
 
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function OtherUserProfile() {
+  // Router
+  const router = useRouter();
+
   // User id
   const { otherUserId } = useLocalSearchParams();
 
@@ -50,19 +56,40 @@ export default function OtherUserProfile() {
   return (
     <GestureHandlerRootView style={gestureHandlerRootViewStyle.container}>
       {/* Header */}
-      <Header user={user} />
+      <View style={otherUserProfileStyles.profileHeaderContainer}>
+        {/* Profile Image */}
+        <TouchableOpacity onPress={() => router.push(`/(private)/userProfilePicture/${user?.uid}`)}>
+          <Image
+            source={{ uri: user?.image }}
+            style={{ width: 80, height: 80, borderRadius: 50 }}
+            placeholder={{ blurhash: blurhash }}
+            contentFit="contain"
+            transition={1000}
+            alt="Profile Picture"
+          />
+        </TouchableOpacity>
+
+        {/* Chat Button */}
+        <TouchableOpacity style={otherUserProfileStyles.chatBtn}>
+          <MaterialCommunityIcons name="chat-outline" size={20} color={COLORS.white} />
+          <Text style={otherUserProfileStyles.chatText}>Chat</Text>
+        </TouchableOpacity>
+      </View>
 
       <ScrollView>
         {/* User Bio */}
         <UserBio isLoading={isLoading} user={user} />
 
-        {/* Posts Button */}
+        {/* Title */}
+        <View style={otherUserProfileStyles.titleContainer}>
+          <Text style={otherUserProfileStyles.titleText}>Posts</Text>
+        </View>
 
         {/* Divider*/}
         <View style={reUseableStyles.bottomSheetDivider} />
 
         {/* Created Posts  */}
-        {!isLoadingCreatedPosts && posts.length > 0 && <PostsList posts={posts} />}
+        {!isLoadingCreatedPosts && <PostsList posts={posts} />}
       </ScrollView>
 
       {/* Bottom Sheet */}
@@ -80,3 +107,46 @@ export default function OtherUserProfile() {
     </GestureHandlerRootView>
   );
 }
+
+const otherUserProfileStyles = StyleSheet.create({
+  profileHeaderContainer: {
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  chatBtn: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 6,
+    backgroundColor: COLORS.darkBlue,
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 5,
+    elevation: 6,
+  },
+
+  chatText: {
+    color: COLORS.lightGrey,
+    fontSize: 14,
+    fontFamily: "Segoe_UI_Bold",
+    textAlign: "center",
+  },
+
+  titleContainer: {
+    backgroundColor: COLORS.purple,
+    borderRadius: 5,
+    padding: 5,
+    marginTop: 10,
+    width: 100,
+    alignSelf: "center",
+  },
+
+  titleText: {
+    color: COLORS.white,
+    fontFamily: "Segoe_UI_Bold",
+    fontSize: 12,
+    textAlign: "center",
+  },
+});
