9aa39ee4da2200e09c4773bc992cc2a6c76e5ca4
Oyabure Zakari <oyabure205@gmail.com>
2026-02-16 10:47:38 +0100

    Dynamically navigate users to either their own profile screen or the profile screen of other users in [PostId] post details screen

--- components/postDetails/PostDetailsUserImage.tsx
+++ components/postDetails/PostDetailsUserImage.tsx
@@ -1,4 +1,5 @@
 import blurhash from "@/constants/expoBlurImage";
+import { useAuth } from "@/contexts/AuthContext";
 import { Image } from "expo-image";
 import { useRouter } from "expo-router";
 import { TouchableOpacity } from "react-native";
@@ -8,10 +9,25 @@ type PostDetailsUserImageProps = {
   userUid: string | undefined;
 };
 
-const PostDetailsUserImage: React.FC<PostDetailsUserImageProps> = ({ postUserImage, userUid }) => {
+const PostDetailsUserImage: React.FC<PostDetailsUserImageProps> = ({
+  postUserImage,
+  userUid: otherUserUid,
+}) => {
   const router = useRouter();
+
+  const { userUid: currentlyLoggedInUser } = useAuth();
+
+  const handleNavigation = () => {
+    if (otherUserUid === currentlyLoggedInUser) {
+      // If the post is owned by the current user then navigate to the Profile screen
+      router.push("/(private)/(tabs)/Profile");
+    } else {
+      // If the post is not owned by the current user then navigate to the OtherUserProfile screen
+      router.push(`/(private)/otherUserProfile/${otherUserUid}`);
+    }
+  };
   return (
-    <TouchableOpacity onPress={() => router.push(`/(private)/userProfilePicture/${userUid}`)}>
+    <TouchableOpacity onPress={handleNavigation}>
       <Image
         source={{ uri: postUserImage }}
         style={{ width: 50, height: 50, borderRadius: 25 }}
