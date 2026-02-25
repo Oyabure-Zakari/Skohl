710f6a4b2f43c834c9abf1f3a1e046a4e20c6f06
Oyabure Zakari <oyabure205@gmail.com>
2026-02-25 12:44:43 +0100

    Enhance message count display formatting in ChatRoom component

--- app/(private)/ChatRoom.tsx
+++ app/(private)/ChatRoom.tsx
@@ -101,6 +101,15 @@ export default function ChatRoom() {
     setMessages((previousMessages) => GiftedChat.append(previousMessages, messages));
   }, []);
 
+  function formatMessageCount(count: number) {
+    if (count < 1000) return count.toString();
+    if (count < 10000) return (count / 1000).toFixed(1) + "k";
+    if (count < 1000000) return Math.round(count / 1000) + "k";
+    return (count / 1000000).toFixed(1) + "M";
+  }
+
+  const messageCount = formatMessageCount(messages.length);
+
   return (
     <>
       <StatusBar style="dark" backgroundColor={COLORS.lightGrey} />
@@ -146,7 +155,7 @@ export default function ChatRoom() {
               color: COLORS.darkBlue,
             }}
           >
-            {`${messages.length}`}
+            {messageCount}
           </Text>
         </View>
       </View>
