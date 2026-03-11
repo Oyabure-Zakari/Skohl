ec126eb36069703a701e628adf101774c61c72d5
Oyabure Zakari <oyabure205@gmail.com>
2026-03-11 17:36:03 +0100

    Make a constant AUDIO object for the newly downloaded sounds

---
+++ constants/audio.ts
@@ -0,0 +1,7 @@
+const AUDIO = {
+  message: require("@/assets/audio/message-send.mp3"),
+  success: require("@/assets/audio/success-sound.mp3"),
+  error: require("@/assets/audio/error-sound.mp3"),
+};
+
+export default AUDIO;
